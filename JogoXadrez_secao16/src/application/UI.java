ff06f1b1fb2eada26a724919a7d1da4eddbe92a3
welington <welington.magalhaes@capgemini.com>
2023-03-16 18:45:05 -0300

    tabuleiro colorido

--- JogoXadrez_secao16/src/application/UI.java
+++ JogoXadrez_secao16/src/application/UI.java
@@ -1,30 +1,68 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
-	
-	//logica para impressão do tabuleiro
+
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+	// para imprimir cores no console
+	// cor do texto
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	//cor do fundo
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
+	// logica para impressão do tabuleiro
 	public static void printBoard(ChessPiece[][] pieces) {
-		for(int i=0; i< pieces.length; i++) {
-			System.out.print( (8-i) + " ");
-			for(int j=0; j< pieces.length; j++) {
-				printPiece(pieces[i][j]);
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
 		System.out.println("  A B C D E F G H");
 	}
-	
-	//para imprimir uma peça
-	private static void printPiece(ChessPiece piece) {
-		if(piece == null) {
-			System.out.print("-");
+
+	// para imprimir uma peça
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
-		else {
-			System.out.print(piece);
-		}
-		
-		System.out.print(" "); //espaço em branco para separar as peças
+    	if (piece == null) {
+            System.out.print("-" + ANSI_RESET);
+        }
+        else {
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET); //usa o amarelo para as peças pretas, pois o fundo da tela é preto
+            }
+        }
+
+		System.out.print(" "); // espaço em branco para separar as peças
 	}
 }
